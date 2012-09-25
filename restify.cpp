82d92e6dd33b180c83c780b5b0d84e9ff3d773ef
Micheal Morgan <micheal@morgan.ly>
2012-09-25 00:37:24 -0400

    Building out layout and options.

--- restify.cpp
+++ restify.cpp
@@ -3,7 +3,20 @@
 
 Restify::Restify()
 {
+	optionsGroupBox = new QGroupBox(tr("Options"));
+
+    url = new QInputDialog;
+    urlLabel = new QLabel(tr("URL"));
+
+	optionsGroupBoxLayout = new QGridLayout; 
+    optionsGroupBoxLayout->addWidget(urlLabel, 0, 0);
+    optionsGroupBoxLayout->addWidget(url, 0, 1);
+	optionsGroupBox->setLayout(optionsGroupBoxLayout);
+
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(optionsGroupBox);
+    setLayout(mainLayout);	
+
     setWindowTitle(tr("Restify"));
-    
-    resize(500, 200);
+    resize(500, 200);    
 }
--- restify.h
+++ restify.h
@@ -3,10 +3,25 @@
 
 #include <QWidget>
 
+QT_BEGIN_NAMESPACE
+class QInputDialog;
+class QGroupBox;
+class QGridLayout;
+class QLabel;
+class QVBoxLayout;
+QT_END_NAMESPACE
+
 class Restify : public QWidget
 {
 	public:
 	    Restify();
+
+	private:
+		QInputDialog *url;
+		QLabel *urlLabel;
+	    QGroupBox *optionsGroupBox;
+	    QGridLayout *optionsGroupBoxLayout;   
+	    QVBoxLayout *mainLayout;
 };
 
 #endif
