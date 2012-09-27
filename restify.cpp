3996597027579110796f36d93ecdb7693cfbfcf4
Micheal Morgan <micheal@morgan.ly>
2012-09-27 00:29:57 -0400

    Added Response UI and Request signal/slot

--- restify.cpp
+++ restify.cpp
@@ -2,20 +2,35 @@
 
 Restify::Restify()
 {
-	setupOptions();
+	this->_setupRequest();
+	this->_setupResponse();
 
 	requestLayout = new QHBoxLayout;
 	requestLayout->addWidget(method);
 	requestLayout->addWidget(url);
 	requestLayout->addWidget(submit);
 
-	setLayout(requestLayout);
+	layout = new QVBoxLayout;
+	layout->addLayout(requestLayout);
+	layout->addWidget(responseLayout);
+
+	setLayout(layout);
 
 	setWindowTitle(tr("Restify"));
-	resize(800, 50);    
+	resize(800, 50);
 }
 
-void Restify::setupOptions()
+QString Restify::getMethod()
+{
+	return method->currentText();
+}
+
+QString Restify::getUrl()
+{
+	return url->text();
+}
+
+void Restify::_setupRequest()
 {
 	method = new QComboBox;
 	method->addItem("GET");
@@ -27,5 +42,23 @@ void Restify::setupOptions()
 	url = new QLineEdit;
 	url->setProperty("placeholderText", "http://api.example.com/resource.json?key=value");
 
-	submit = new QPushButton("Request");
+	submit = new QPushButton(tr("Request"));
+	submit->connect(submit, SIGNAL(clicked()), this, SLOT(_request()));
+}
+
+void Restify::_setupResponse()
+{
+	responseContent = new QTextEdit;
+	responseContent->setProperty("readOnly", true);
+
+	responseLayout = new QTabWidget;
+	responseLayout->hide();
+	responseLayout->addTab(responseContent, tr("Response"));
+}
+
+void Restify::_request()
+{
+	responseContent->setText(this->getMethod() + " " + this->getUrl());
+
+	responseLayout->show();
 }
--- restify.h
+++ restify.h
@@ -3,22 +3,36 @@
 
 #include <QWidget>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QComboBox>
 #include <QLineEdit>
 #include <QPushButton>
+#include <QTabWidget>
+#include <QTextEdit>
 
 class Restify : public QWidget
 {
+	Q_OBJECT
+
 	public:
 		Restify();
+		QString getMethod();
+		QString getUrl();
+
+	private slots:
+		void _request();
 
 	private:
-		void setupOptions();
+		void _setupRequest();
+		void _setupResponse();
 
 		QHBoxLayout *requestLayout;
+		QVBoxLayout *layout;
 		QComboBox *method;
 		QLineEdit *url;
 		QPushButton *submit;
+		QTabWidget *responseLayout;
+		QTextEdit *responseContent;
 };
 
 #endif
